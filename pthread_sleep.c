c7a9a0f82637b1c6d009b69fb1949cb0eed30c15
Kaan Türkmen <kaanturkmen@protonmail.com>
2021-05-24 17:37:12 +0300

    pthread_sleep now takes double values as a parameter.

--- pthread_sleep.c
+++ pthread_sleep.c
@@ -1,34 +1,40 @@
- /****************************************************************************** 
-  pthread_sleep takes an integer number of seconds to pause the current thread 
-  original by Yingwu Zhu
-  updated by Muhammed Nufail Farooqi
-  *****************************************************************************/
-int pthread_sleep (int seconds)
-{
-   pthread_mutex_t mutex;
-   pthread_cond_t conditionvar;
-   struct timespec timetoexpire;
-   if(pthread_mutex_init(&mutex,NULL))
-    {
-      return -1;
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+
+/**
+ * pthread_sleep takes an integer number of seconds to pause the current thread
+ * original by Yingwu Zhu
+ * updated by Muhammed Nufail Farooqi
+ * updated by Fahrican Kosar
+ */
+int
+pthread_sleep(double seconds){
+    pthread_mutex_t mutex;
+    pthread_cond_t conditionvar;
+    if(pthread_mutex_init(&mutex,NULL)){
+        return -1;
     }
-   if(pthread_cond_init(&conditionvar,NULL))
-    {
-      return -1;
+    if(pthread_cond_init(&conditionvar,NULL)){
+        return -1;
     }
-   struct timeval tp;
-   //When to expire is an absolute time, so get the current time and add //it to our delay time
-   gettimeofday(&tp, NULL);
-   timetoexpire.tv_sec = tp.tv_sec + seconds; timetoexpire.tv_nsec = tp.tv_usec * 1000;
 
-   pthread_mutex_lock (&mutex);
-   int res =  pthread_cond_timedwait(&conditionvar, &mutex, &timetoexpire);
-   pthread_mutex_unlock (&mutex);
-   pthread_mutex_destroy(&mutex);
-   pthread_cond_destroy(&conditionvar);
+    struct timeval tp;
+    struct timespec timetoexpire;
+    // When to expire is an absolute time, so get the current time and add
+    // it to our delay time
+    gettimeofday(&tp, NULL);
+    long new_nsec = tp.tv_usec * 1000 + (seconds - (long)seconds) * 1e9;
+    timetoexpire.tv_sec = tp.tv_sec + (long)seconds + (new_nsec / (long)1e9);
+    timetoexpire.tv_nsec = new_nsec % (long)1e9;
 
-   //Upon successful completion, a value of zero shall be returned
-   return res;
+    pthread_mutex_lock(&mutex);
+    int res = pthread_cond_timedwait(&conditionvar, &mutex, &timetoexpire);
+    pthread_mutex_unlock(&mutex);
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&conditionvar);
 
+    //Upon successful completion, a value of zero shall be returned
+    return res;
 }
-
